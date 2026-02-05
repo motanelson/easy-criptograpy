e40f171d407573e48b07ed7326b27453029af87c
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-02-05 22:25:05 +0000

    Add files via upload

--- reverter.py
+++ reverter.py
@@ -1,21 +1,16 @@
-print( "\033c\033[40;37m\give the file .txt reverter ? ")
-a="vr.txt"
-#a=input()
-print( "\033c\033[40;37m\give the file .dat reverter ? ")
-b="vr.dat"
-#b=input()
-f1=open(a,"r")
-z=f1.read()
-f1.close()
-x=z.split("\n")
-n=""
-m=""
-for y in x:
-    n=""
-    for g in y:
-        n=g+n
-    m=n+"\n"+m
-f1=open(b,"w")
-f1.write(m)
-f1.close()
+print("\033[40;37m\ngive the file .txt reverter ? ")
+a = input().strip()
 
+print("\033[40;37m\ngive the file .dat reverter ? ")
+b = input().strip()
+
+with open(a, "rb") as f:
+    data = f.read()
+
+out = bytearray()
+
+for byte in data:
+    out.insert(0, (~byte) & 0xFF)   # NOT + reverse
+
+with open(b, "wb") as f:
+    f.write(out)
